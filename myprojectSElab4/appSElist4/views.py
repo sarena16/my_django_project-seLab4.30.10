8cfb41708e907a694de2b3233a5eb6eb3e5ddd52
Sara <276485@student.pwr.edu.pl>
2024-11-25 13:37:31 +0100

    Part 3 completed

--- myprojectSElab4/appSElist4/views.py
+++ myprojectSElab4/appSElist4/views.py
@@ -1,16 +1,43 @@
 from rest_framework import viewsets
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Product, Customer, Order
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
+from .forms import ProductForm
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
-class CustomerViewSet(viewsets.ModelViewSet):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 #
+#
+# class CustomerViewSet(viewsets.ModelViewSet):
+#     queryset = Customer.objects.all()
+#     serializer_class = CustomerSerializer
+#
+#
+# class OrderViewSet(viewsets.ModelViewSet):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'product_create.html'
+    success_url = '../../products/'  
+
+    def form_valid(self, form):
+        return super().form_valid(form)
