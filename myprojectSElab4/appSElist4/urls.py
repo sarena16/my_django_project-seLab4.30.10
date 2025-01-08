39a5d29e1bdd1fdee25074465a35640a79394474
Sara <276485@student.pwr.edu.pl>
2025-01-08 10:05:43 +0100

    List 9 - Part 1 completed

--- myprojectSElab4/appSElist4/urls.py
+++ myprojectSElab4/appSElist4/urls.py
@@ -1,7 +1,38 @@
+# from django.urls import path, include
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework.routers import DefaultRouter
+# from .views import ProductViewSet, CustomerViewSet, OrderViewSet
+#
+# router = DefaultRouter()
+# router.register(r'products', ProductViewSet, basename='product')
+# router.register(r'customers', CustomerViewSet, basename='customer')
+# router.register(r'orders', OrderViewSet, basename='order')
+#
+# urlpatterns = [
+#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     path('api/', include(router.urls)),
+# ]
+
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, CustomerViewSet, OrderViewSet
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Software Engineering Lab",
+        default_version="v1",
+        description="API documentation for the lab",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
+)
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -12,5 +43,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-#
--- myprojectSElab4/myprojectSElab4/settings.py
+++ myprojectSElab4/myprojectSElab4/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'appSElist4',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
