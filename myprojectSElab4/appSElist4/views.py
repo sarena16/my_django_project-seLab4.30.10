ecc0a8fd2cdd9ae64ca9a6ec2589346e72be3906
Sara <276485@student.pwr.edu.pl>
2025-01-28 19:27:32 +0100

    changes from list 9 part 3 point 1

--- myprojectSElab4/appSElist4/views.py
+++ myprojectSElab4/appSElist4/views.py
@@ -3,11 +3,16 @@ from .models import Product, Customer, Order
 from .serializers import ProductSerializer, CustomerSerializer, OrderSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminOrReadOnly
+from rest_framework.filters import SearchFilter
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    filter_backends = (SearchFilter,)
+    search_fields = ['name']
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
